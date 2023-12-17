e932e02d1166a6fa72af120260d221ac554067e6
viethungle1 <hunglv0801@gmail.com>
2023-12-17 16:27:26 +0700

    done

--- Week2/src/Main.java
+++ Week2/src/Main.java
@@ -1,61 +1,48 @@
-
 public class Main {
     public static void main(String[] args) {
-//      Tạo 5 cuốn sách ProgrammingBook và 5 cuốn FictionBook
-        System.out.println("a)");
-        int sumP=0;
-        Book[]arrProgramming = new Book[5];
-        for (int i = 0; i < arrProgramming.length; i++) {
-            arrProgramming[i] = new ProgrammingBook();
-            arrProgramming[i].setPrice(Math.round(Math.random()*(90-70+1)+70));
-            System.out.println("Giá sách lập trình thứ " + i + " là " + arrProgramming[i].getPrice()+"$");
-            sumP += arrProgramming[i].getPrice();
+        ProgrammingBook[] programmingBooks = new ProgrammingBook[5];
+        programmingBooks[0] = new ProgrammingBook("P1", "Programming1", 50, "A", "Java", "1");
+        programmingBooks[1] = new ProgrammingBook("P2", "Programming2", 60, "B", "NodeJS", "2");
+        programmingBooks[2] = new ProgrammingBook("P3", "Programming3", 70, "C", "C++", "3");
+        programmingBooks[3] = new ProgrammingBook("P4", "Programming4", 80, "D", "Pascal", "4");
+        programmingBooks[4] = new ProgrammingBook("P5", "Programming5", 90, "E", "Python", "5");
+        FictionBook[] fictionBooks = new FictionBook[5];
+        fictionBooks[0] = new FictionBook("F1", "Fiction1", 80, "F", "Viễn tưởng 1");
+        fictionBooks[1] = new FictionBook("F2", "Fiction2", 90, "F", "Viễn tưởng 2");
+        fictionBooks[2] = new FictionBook("F3", "Fiction3", 100, "F", "Viễn tưởng 3");
+        fictionBooks[3] = new FictionBook("F4", "Fiction4", 110, "F", "Viễn tưởng 4");
+        fictionBooks[4] = new FictionBook("F5", "Fiction5", 120, "F", "Viễn tưởng 5");
+
+        double sum = 0;
+        double sumP = 0;
+        double sumF = 0;
+        for (int i = 0; i < programmingBooks.length; i++) {
+            sumF += programmingBooks[i].getPrice();
         }
-        System.out.println("--------------------------------");
-        int sumF = 0;
-        Book[]arrFiction = new Book[5];
-        for (int i = 0; i < arrFiction.length ; i++) {
-            arrFiction[i] = new FictionBook();
-            arrFiction[i].setPrice(Math.round(Math.random()*(110-90+1)+90));
-            System.out.println("Giá sách viễn tưởng thứ " + i + " là " + arrFiction[i].getPrice()+"$");
-            sumF += arrFiction[i].getPrice();
+        for (int i = 0; i < fictionBooks.length; i++) {
+            sumF += fictionBooks[i].getPrice();
         }
-        System.out.println("--------------------------------");
-        System.out.println("b)");
-//      Tính tổng tiền 10 cuốn sách
-        double sum=0;
-        sum = sumF+sumP;
-        System.out.println("Tổng giá 10 cuốn sách là " + sum +"$");
-        System.out.println("--------------------------------");
-        System.out.println("c)");
-//      Đếm số sách ProgrammingBook có languague là Java
-        int countProgramming = 0;
-        for (int i = 0; i < arrProgramming.length; i++) {
-            ProgrammingBook a = (ProgrammingBook) arrProgramming[i];
-            if (arrProgramming[i].getPrice()>80) {
-                ((ProgrammingBook) arrProgramming[i]).setLanguage("java");
-                countProgramming++;
+        sum = sumP + sumF;
+        System.out.println("Giá 10 cuốn sách: " + sum);
+        int count1 = 0;
+        for (int i = 0; i < programmingBooks.length; i++) {
+            if (programmingBooks[i].getLanguage().equals("Java")) {
+                count1++;
             }
         }
-        System.out.println("số sách có language Java là: " +countProgramming);
-//      Đếm số sách FictionBook có category là Viễn tưởng 1
-        System.out.println("--------------------------------");
-        System.out.println("d)");
-        int countFiction = 0;
-        int countFiction2= 0;
-        for (int i = 0; i < arrFiction.length; i++) {
-            FictionBook b = (FictionBook) arrFiction[i];
-            if (arrFiction[i].getPrice()>100) {
-                ((FictionBook) arrFiction[i]).setCategory("Viễn tưởng 1");
-                countFiction++;
+        System.out.println("Số sách ngôn ngữ Java: "+count1);
+        int count2 = 0;
+        int count3 = 0;
+        for (int i = 0; i < fictionBooks.length; i++) {
+            if (fictionBooks[i].getCategory().equals("Viễn tưởng 1")) {
+                count2++;
             }
-            if (arrFiction[i].getPrice()<100) {
-                countFiction2++;
+            if (fictionBooks[i].getPrice()<100) {
+                count3++;
             }
         }
-        System.out.println("số sách có category (Viễn tưởng 1) là: " +countFiction);
-        System.out.println("--------------------------------");
-        System.out.println("e)");
-        System.out.println("số sách FictionBook có giá <100 là: " +countFiction2);
+        System.out.println("Số sách category Viễn tưởng 1: "+count2);
+        System.out.println("Số sách viễn tưởng giá nhỏ hơn 100: "+count3);
     }
 }
+
